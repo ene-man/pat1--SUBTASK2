465cdbccf32da7ce9e2ce1f98af8cdfd9ccce83c
Eli-Nathan-designer <nathanelieastwood@gmail.com>
2026-04-17 12:11:44 +0200

    added input temperture

--- Magwinya.cpp
+++ Magwinya.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int temp1, temp2, temp3;
 
-    // Input temperatures
+    // input temperture
     cout << "Enter the first temperature reading: ";
     cin >> temp1;
 
